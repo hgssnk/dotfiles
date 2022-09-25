1a4591ae18584d5332b61294036cb612441a354b
hgssnk <60138403+hgssnk@users.noreply.github.com>
2022-09-25 15:30:01 +0900

    Delete script_touchoff.bat

--- .config/shortcut/public/script_touchoff.bat
+++
@@ -1,4 +0,0 @@
-rem ## タッチパッドスクリーンと、タッチペンを無効化する
-rem ## デバイスインスタンスパスを確認すること（参考：https://pmp-style.hatenablog.com/entry/PowerShell_DeviceManage）
-pnputil /disable-device "HID\NTRG0001&COL02\5&63F74D3&0&0001"
-pnputil /disable-device "HID\NTRG0001&COL01\5&63F74D3&0&0000"
